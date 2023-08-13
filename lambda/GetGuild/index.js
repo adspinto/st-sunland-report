efe6be13ac32723c81dc08a7d668f3d6e09a5176
Andre <andre.pinto@metaltoad.com>
2023-08-12 22:45:25 -0300

    updated GetGuild lambda

--- lambda/GetGuild/index.js
+++ lambda/GetGuild/index.js
@@ -1,14 +1,97 @@
 import AWS from "aws-sdk";
 const docClient = new AWS.DynamoDB.DocumentClient();
 
+const investPerLevel = [
+  {
+    levelRange: 40,
+    value: 75,
+  },
+  {
+    levelRange: 50,
+    value: 150,
+  },
+  {
+    levelRange: 60,
+    value: 250,
+  },
+  {
+    levelRange: 70,
+    value: 400,
+  },
+];
+
+function isMonday() {
+  return new Date().getDay() === 1;
+}
+
 const parseDynamoItem = (item) => {
   const obj = {};
   Object.keys(item).forEach((key) => {
-    Object.assign(obj, {
-      [key]: { N: item[key] },
-    });
+    const value = item[key];
+
+    const assigned = {
+      [key]: value,
+    };
+
+    if (key == "_id") {
+      assigned.playerId = item[key];
+    }
+
+    if (key == "name") {
+      assigned.playerName = item[key];
+    }
+
+    assigned.updatedAt = Math.floor(Date.now() / 1000);
+
+    Object.assign(obj, assigned);
   });
-  return obj;
+  return {
+    PutRequest: {
+      Item: obj,
+    },
+  };
+};
+
+const batchWrite = async (res) => {
+  const shouldWrite = isMonday();
+  if (shouldWrite) {
+    const data = res.data.members.map((item) => parseDynamoItem(item));
+    const params = {
+      RequestItems: {
+        [process.env.TABLE_NAME]: data,
+      },
+    };
+
+    await docClient.batchWrite(params).promise();
+  }
+};
+
+const scan = async (res) => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+  };
+  const items = await docClient.scan(params).promise();
+  return res.data.members.map((item) => {
+    try {
+      const find = items.Items.find((value) => value._id === item._id);
+      const state = {
+        ...item,
+      };
+      if (find) {
+        state.invst_monday = find.invst;
+        const diffInvest = item.invst - find.invst;
+        const findLevel = investPerLevel.find(
+          (value) => item.level - value.levelRange < 10
+        );
+        state.percent_invested = findLevel.value / diffInvest;
+      }
+
+      return state;
+    } catch (error) {
+      console.log("error", error);
+      return item;
+    }
+  });
 };
 
 export const handler = async (event) => {
@@ -21,24 +104,14 @@ export const handler = async (event) => {
   try {
     const apiRes = await fetch(`${apiUrl}/info/city/${guildId}`);
     const res = await apiRes.json();
-
-    const data = res.data.members.map((item) => parseDynamoItem(item));
-    const params = {
-      RequestItems: {
-        [process.env.TABLE_NAME]: {
-          Keys: data,
-          ProjectionExpression: "KEY_NAME, ATTRIBUTE",
-        },
-      },
-    };
-
-    await docClient.batchWrite(params).promise();
-    response.body = JSON.stringify(res);
+    await batchWrite(res);
+    const data = await scan(res);
+    response.body = JSON.stringify(data);
   } catch (error) {
     response.statusCode = 400;
     response.body = JSON.stringify({
       message: "Could not perform the operation, try again.",
-      error: error,
+      error: JSON.stringify(error),
     });
   }
   return response;
