39ec6904bf35daacd7bfe460724fa8071cf91aaa
Andre <andre.pinto@metaltoad.com>
2023-10-01 11:17:11 -0300

    update weekly report

--- lambda/UpdateWeeklyReport/index.js
+++ lambda/UpdateWeeklyReport/index.js
@@ -1,36 +1,20 @@
 import AWS from "aws-sdk";
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-const parseDynamoItemsGET = (res, tableName) => {
-  const items = res.data.members.map((member) => {
-    return {
-      playerId: member._id,
-      playerName: member.name,
-    };
-  });
-
-  return {
-    RequestItems: {
-      [tableName]: {
-        Keys: items,
-      },
-    },
-    ConsistentRead: false,
-  };
-};
-
-const getPreviousData = async (res, tableName) => {
+const getPreviousDataByScan = async (guildId) => {
   try {
-    const params = parseDynamoItemsGET(res, tableName);
-    const items = await docClient.batchGet(params).promise();
-    return items.Responses[tableName];
+    const params = {
+      TableName: guildId,
+    };
+    const response = await docClient.scan(params).promise();
+    return response.Items;
   } catch (error) {
     console.log("error", error);
   }
 };
-
 const parseDynamoItem = (item) => {
   const obj = {};
+
   Object.keys(item).forEach((key) => {
     const value = item[key];
 
@@ -45,35 +29,68 @@ const parseDynamoItem = (item) => {
     if (key == "name") {
       assigned.playerName = item[key];
     }
-
+    
+    if(key === "invst") {
+      assigned.invst_monday = value;  
+    }
+    
+    if(key === "bounty") {
+      assigned.bounty_monday = value;
+    }
+    
     assigned.updatedAt = Math.floor(Date.now() / 1000);
 
     Object.assign(obj, assigned);
   });
 
-  return {
+  const params = {
     PutRequest: {
       Item: obj,
     },
   };
+
+  return params;
 };
 
-const batchWrite = async (res, guildId, previousData) => {
-  // const dataToDelete = previousData.filter()
-  const data = res.data.members.map((item) => parseDynamoItem(item));
+const batchWrite = async (res, guildId) => {
+  const data = res.data.members.map((item) => {
+    return parseDynamoItem(item);
+  });
+  const params = {
+    RequestItems: {
+      [guildId]: data,
+    },
+  };
+  console.log(JSON.stringify(params))
+  await docClient.batchWrite(params).promise();
+};
+
+const batchDelete = async (res, guildId, previousData) => {
+  const ids = res.data.members.map((item) => item._id);
+  const data = previousData
+    .filter((pItem) => !ids.includes(pItem._id))
+    .map((item) => {
+      return {
+        DeleteRequest: {
+          Key: { playerId: item.playerId, playerName: item.playerName },
+        },
+      };
+    });
   const params = {
     RequestItems: {
       [guildId]: data,
     },
   };
 
-  await docClient.batchWrite(params).promise();
+  if (data.length > 0) {
+    await docClient.batchWrite(params).promise();
+  }
 };
 
 export const handler = async (event) => {
   const apiUrl = process.env.SMARTY_API_URL;
   const guildId = event.queryStringParameters.guildId;
-  const gId = event.queryStringParameters.gId;
+  const gId = event?.queryStringParameters?.gId;
   console.log("guildId", guildId);
   const response = {
     statusCode: 200,
@@ -89,14 +106,18 @@ export const handler = async (event) => {
     const apiRes = await fetch(`${apiUrl}/info/city/${gId || guildId}`);
     console.log("getting smarty data");
     const res = await apiRes.json();
+    console.log("get previousData");
+    const previousData = await getPreviousDataByScan(guildId);
+    console.log("batch write start");
+    await batchWrite(res, guildId);
+    console.log("batch write finished");
+    console.log("batch delete start");
+    await batchDelete(res, guildId, previousData);
+    console.log("batch delete finish");
 
-    const previousData = await getPreviousData(res, guildId);
-
-    console.log("start batch write", previousData);
-    // await batchWrite(res, guildId, previousData);
-    // console.log("batch write finished");
-    // response.body = JSON.stringify({ message: "Write completed!" });
+    response.body = JSON.stringify({ message: "Write and delete completed!" });
   } catch (error) {
+    console.log("error in the first catch", error);
     response.statusCode = 400;
     response.body = JSON.stringify({
       message: "Could not perform the operation, try again.",
